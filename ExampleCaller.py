880692f4e11fd25e6018a486296159a8cd84e770
Claire Medeiros <cm@evident.io>
2016-01-21 23:27:06 +0000

    init

---
+++ ExampleCaller.py
@@ -0,0 +1,9 @@
+from Evident import EvidentApi
+
+secretKey = '[Secret Key]'
+publicKey = '[Public Key]'
+evidentApi = EvidentApi(secretKey, publicKey)
+evidentApi.ListSignatures()
+evidentApi.ListSeppressions()
+evidentApi.ShowSeppression(id = '3715')
+evidentApi.CreateSignatureSeppression(signature_ids = ['26'], resource = 'i-dce5491a', external_account_ids = ['1968'])
