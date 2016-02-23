1f233639bf1837a7e3ba90e88a76e82e8c0da32e
Kyle Terry <kyle@kyleterry.com>
2016-02-23 12:33:53 -0800

    Fixing api authentication. Digest date was wrong

--- esp/auth.py
+++ esp/auth.py
@@ -5,15 +5,25 @@ import hmac
 from time import mktime
 from urllib.parse import urlparse
 from wsgiref.handlers import format_date_time
+from requests.auth import AuthBase
 
 CONTENT_TYPE = 'application/vnd.api+json'
 
 
-class ESPAuth(object):
+class UnauthorizedError(Exception):
+    pass
+
+
+class ESPAuth(AuthBase):
+    """
+    An instance of this class should be passed into a requests call as
+    the `auth` parameter.
+    """
 
     def __call__(self, r):
         """
-        :param r: requests.Request
+        :param r: requests Request object instance
+        :type r: requests.Request
         """
         now = datetime.now()
         tt = mktime(now.timetuple())
@@ -27,8 +37,10 @@ class ESPAuth(object):
 
     def __init__(self, access_key_id, secret_access_key):
         """
-        :param access_key_id: string
-        :param secret_access_key: string
+        :param access_key_id: public access key from ESP
+        :type access_key_id: string
+        :param secret_access_key: secret access key from ESP
+        :type secret_access_key: string
         """
         self.access_key_id = access_key_id
         self.secret_access_key = secret_access_key
@@ -41,24 +53,30 @@ class ESPAuth(object):
         Returns a base64 string of the content body passed in.
         """
         if not body:
-            body = ''
+           body = ''
         body_bytes = body.encode('utf-8')
         b64 = base64.b64encode(hashlib.md5(body_bytes).digest())
-        return b64
+        return b64.decode()
 
     def sign_request(self, r):
         """
-        :type: string
+        :param r: requests Request object instance
+        :type r: requests.Request
+        :returns: str
+
+        Sign request takes pieces of information about the request and
+        generates a hmac hash digest for authentication. This will return a
+        base64 string of the digest
         """
         url = urlparse(r.url)
-        cannonical = '{content_type},{md5},{uri},{date}'.format(
+        canonical = '{content_type},{md5},{uri},{date}'.format(
             content_type=CONTENT_TYPE,
             md5=self.body_md5(r.body),
             uri=url.path,
             date=self.date)
-        h = hmac.new(self.secret_access_key.encode('utf-8'),
-                     cannonical.encode('utf-8'),
-                     hashlib.sha1)
+        digest = hmac.new(self.secret_access_key.encode('utf-8'),
+                     canonical.encode('utf-8'),
+                     digestmod=hashlib.sha1).digest()
         return 'APIAuth {access_key}:{signature}'.format(
             access_key=self.access_key_id,
-            signature=base64.b64encode(h.digest()).strip())
+            signature=base64.b64encode(digest).decode())
