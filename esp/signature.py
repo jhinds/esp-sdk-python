c79365498c368149462229bbbe93012412ff4cd5
Kyle Terry <kyle@kyleterry.com>
2016-03-04 10:49:11 -0800

    building out custom run method on signatures

--- esp/signature.py
+++ esp/signature.py
@@ -1,4 +1,6 @@
-from .resource import ESPResource
+from .alert import Alert
+from .resource import ESPResource, POST_REQUEST, PaginatedCollection
+from .sdk import make_endpoint
 
 
 class Signature(ESPResource):
@@ -13,5 +15,12 @@ class Signature(ESPResource):
     def destroy(self):
         raise NotImplementedError('Alert does not implement a destroy method')
 
-    def run(self):
-        pass
+    def run(self, external_account_id, region):
+        self.external_account_id = external_account_id
+        self.region = region
+        endpoint = make_endpoint(self._resource_path(self.id_, extra=['run']))
+        response = self._make_request(endpoint,
+                                      POST_REQUEST,
+                                      data=self.to_json())
+        data = response.json()
+        return PaginatedCollection(Alert, data)
