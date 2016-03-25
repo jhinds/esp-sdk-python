034e7d3b971b96d9fbda738285c0f7939dbec557
Kyle Terry <kyle@kyleterry.com>
2016-03-25 11:08:58 -0700

    tags by alert and overriding where as a not implemented error

--- esp/tag.py
+++ esp/tag.py
@@ -1,4 +1,5 @@
 from .resource import ESPResource
+from .sdk import make_endpoint
 
 
 class Tag(ESPResource):
@@ -7,8 +8,18 @@ class Tag(ESPResource):
     def create(cls):
         raise NotImplementedError('Tag does not implement a create method')
 
+    @classmethod
+    def where(cls, **kwargs):
+        raise NotImplementedError('Tag does not implement a where method')
+
     def save(self):
         raise NotImplementedError('Tag does not implement a save method')
 
     def destroy(self):
         raise NotImplementedError('Tag does not implement a destroy method')
+
+    @classmethod
+    def for_alert(cls, alert_id):
+        path = '/'.join(['alerts', str(alert_id), 'tags'])
+        endpoint = make_endpoint(path)
+        return cls._all(endpoint=endpoint)
