2aa5aec88ca2a3767be39dbe2f400dc4aaa29639
Kyle Terry <kyle@kyleterry.com>
2016-03-25 11:02:51 -0700

    overriding find and implementing for_alert for cloud_trail_events

--- esp/cloud_trail_event.py
+++ esp/cloud_trail_event.py
@@ -1,4 +1,5 @@
 from .resource import ESPResource
+from .sdk import make_endpoint
 
 
 class CloudTrailEvent(ESPResource):
@@ -11,8 +12,20 @@ class CloudTrailEvent(ESPResource):
     def where(cls, *args, **kwargs):
         raise NotImplementedError('CloudTrailEvent does not implement a where method')
 
+    @classmethod
+    def find(cls, id):
+        # override find because this class doesn't support listing without
+        # an alert id
+        return super(CloudTrailEvent, cls).find(id=id)
+
     def save(self):
         raise NotImplementedError('CloudTrailEvent does not implement a save method')
 
     def destroy(self):
         raise NotImplementedError('CloudTrailEvent does not implement a destroy method')
+
+    @classmethod
+    def for_alert(cls, alert_id):
+        path = '/'.join(['alerts', str(alert_id), 'cloud_trail_events'])
+        endpoint = make_endpoint(path)
+        return cls._all(endpoint=endpoint)
